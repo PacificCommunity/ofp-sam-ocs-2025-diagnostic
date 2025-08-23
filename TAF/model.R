36571c6674b508cde91fa8d6c0874665625394f9
Arni Magnusson <thisisarni@gmail.com>
2025-08-23 13:00:44 +1100

    Run model

--- TAF/model.R
+++ TAF/model.R
@@ -1,9 +1,25 @@
 # Run analysis, write model results
 
-# Before:
-# After:
+# Before: ss3, ss3.exe (boot/software/ss3),
+#         forecast.ss, OCS.ctl, OCS.dat, starter.ss (boot/data)
+# After:  CompReport.sso, covar.sso, Forecast-report.sso, Report.sso,
+#         warning.sso, wtatage.ss_new (model)
 
 library(TAF)
 
 mkdir("model")
 
+# Get model executable
+exefile <- if(os.linux()) "ss3" else "ss3.exe"
+cp(file.path("boot/software/ss3", exefile), "model")
+
+# Get model input files
+cp("boot/data/starter.ss", "model")
+cp("boot/data/forecast.ss", "model")
+cp("boot/data/OCS.ctl", "model")
+cp("boot/data/OCS.dat", "model")
+
+# Run model
+setwd("model")
+system("./ss3")
+setwd("..")
