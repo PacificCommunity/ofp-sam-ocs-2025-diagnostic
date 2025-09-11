dcc2b65f900c9fefc20a4637d0eb37e6d3a1c748
Arni Magnusson <thisisarni@gmail.com>
2025-09-11 14:23:29 +1100

    Rename Bin -> Length

--- TAF/output_fit.R
+++ TAF/output_fit.R
@@ -13,12 +13,12 @@ mkdir("output")
 model <- SS_output("model", verbose=FALSE, printstats=FALSE)
 
 # CPUE
-cpue <- model$cpue
-cpue <- cpue[c("Yr", "Obs", "Exp", "SE")]
+cpue <- model$cpue[c("Yr", "Obs", "Exp", "SE")]
 
 # Length comps
 length.comps <- model$lendbase
 length.comps <- length.comps[c("Yr", "Fleet", "Bin", "Obs", "Exp", "Nsamp_adj")]
+names(length.comps)[names(length.comps) == "Bin"] <- "Length"
 
 # Write tables
 write.taf(cpue, dir="output")
