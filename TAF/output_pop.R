12a55afb8030cfdd3973b225744499b7e9461f78
Arni Magnusson <thisisarni@gmail.com>
2025-09-11 14:35:30 +1100

    Adapt output_pop to oceanic whitetip

--- TAF/output_pop.R
+++ TAF/output_pop.R
@@ -1,77 +1,61 @@
 # Extract population results, write CSV output tables
 
 # Before: model.rds (model)
-# After:  batage.csv, fatage.csv, natage.csv, stock_area.csv,
-#         summary.csv (output)
+# After:  batage.csv, fatage.csv, natage.csv, summary.csv (output)
 
 library(TAF)
+library(r4ss)
 
 mkdir("output")
 
 # Read model results
-model <- readRDS("model/model.rds")
-annual <- model$annual_time_series
+model <- SS_output("model", verbose=FALSE, printstats=FALSE)
 batage <- model$batage[model$batage$Era == "TIME",]
 derived <- model$derived_quants
-dynamic <- model$Dynamic_Bzero[model$Dynamic_Bzero$Era == "TIME",]
-m.area <- model$M_by_area
+m.area <- model$M_by_area[model$M_by_area$Era == "TIME",]
 natage <- model$natage[model$natage$Era == "TIME",]
+sprseries <- model$sprseries[model$sprseries$Yr <= model$endyr,]
 timeseries <- model$timeseries[model$timeseries$Era == "TIME",]
-z.area <- model$Z_by_area
+z.area <- model$Z_by_area[model$Z_by_area$Era == "TIME",]
 
 # B at age
-batage <- batage[batage$Seas == 1,]
 batage <- batage[batage$"Beg/Mid" == "B",]
-batage <- batage[batage$BirthSeas == 1,]
-batage <- batage[c("Area", "Sex", "Yr", grepv("[0-9]", names(batage)))]
+batage <- batage[c("Sex", "Yr", grepv("[0-9]", names(batage)))]
 batage <- wide2long(batage, names=c("Age", "B"))
 
 # F at age
-exclude <- c("Bio_Pattern", "BirthSeas", "Settlement", "Platoon", "Morph",
-             "Time", "Beg/Mid", "Era")
-m.area <- m.area[m.area$Era == "TIME" & m.area$BirthSeas == 1,]
+exclude <- c("Area", "Bio_Pattern", "BirthSeas", "Settlement", "Platoon",
+             "Morph", "Time", "Beg/Mid", "Era")
 m.area <- m.area[!names(m.area) %in% exclude]
-z.area <- z.area[z.area$Era == "TIME" & z.area$BirthSeas == 1,]
 z.area <- z.area[!names(z.area) %in% exclude]
 m.area <- wide2long(m.area)
 z.area <- wide2long(z.area)
-m.area <- aggregate(Value~Area+Sex+Yr+Age, m.area, mean)
-z.area <- aggregate(Value~Area+Sex+Yr+Age, z.area, mean)
+m.area <- aggregate(Value~Sex+Yr+Age, m.area, mean)
+z.area <- aggregate(Value~Sex+Yr+Age, z.area, mean)
 fatage <- z.area
 fatage$Value <- z.area$Value - m.area$Value
 names(fatage)[names(fatage) == "Value"] <- "F"
 
 # N at age
-natage <- natage[natage$Seas == 1,]
 natage <- natage[natage$"Beg/Mid" == "B",]
-natage <- natage[natage$BirthSeas == 1,]
-natage <- natage[c("Area", "Sex", "Yr", grepv("[0-9]", names(natage)))]
+natage <- natage[c("Sex", "Yr", grepv("[0-9]", names(natage)))]
 natage <- wide2long(natage, names=c("Age", "N"))
 
-# Stock by area
-stock.area <- timeseries[timeseries$Seas == 1,]
-names(stock.area)[names(stock.area) == "Recruit_0"] <- "Rec"
-names(stock.area)[names(stock.area) == "Bio_all"] <- "TB"
-names(stock.area)[names(stock.area) == "SpawnBio"] <- "SB"
-stock.area <- stock.area[c("Area", "Yr", "Rec", "TB", "SB")]
-row.names(stock.area) <- NULL
-
 # Summary
-Year <- annual$year
-Rec <- annual$recruits
-Catch <- annual$dead_catch_B_an
-TB <- annual$Bio_all_an
-SB <- annual$SSB
-Fmort <- annual$"F=Z-M"
+Year <- timeseries$Yr
+Rec <- timeseries$Recruit_0
+Catch <- sprseries$Dead_Catch_B
+TB <- timeseries$Bio_all
+SB <- timeseries$SpawnBio
+Fmort <- sprseries$"F=Z-M"
+SB_SB0 <- SB / derived$Value[derived$Label == "SSB_Virgin"]
 SB_SBmsy <- SB / derived$Value[derived$Label == "SSB_MSY"]
-SB_SBF0 <- SB / dynamic$SSB_nofishing
 F_Fmsy <- Fmort / derived$Value[derived$Label == "annF_MSY"]
-summary <- data.frame(Year, Rec, Catch, TB, SB, F=Fmort, SB_SBmsy, SB_SBF0,
+summary <- data.frame(Year, Rec, Catch, TB, SB, F=Fmort, SB_SB0, SB_SBmsy,
                       F_Fmsy)
 
 # Write tables
 write.taf(batage, dir="output")
 write.taf(fatage, dir="output")
 write.taf(natage, dir="output")
-write.taf(stock.area, dir="output")
 write.taf(summary, dir="output")
